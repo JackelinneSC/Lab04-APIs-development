a4d2fa261e11f696c73e3b85a8625e6016b8cbdb
SC_Jackie <jackiesalazar20@gmail.com>
2017-11-08 23:58:00 -0600

    create a new pizza and insert it to the db, done.

--- testApi/routes/pizza.js
+++ testApi/routes/pizza.js
@@ -1,55 +1,47 @@
 const express = require('express');
 const router = express.Router();
 var pizzap = require('./PizzaModel'); //schema hace todo
-var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 var url = 'mongodb://admin:password20@ds249575.mlab.com:49575/pizzas';
 mongoose.connect(url);
+let actualPizzas = [];
 
 
 router.get('/', function(req, res, next) {
     res.render('createPizza', { title: "JO's Pizza"});
 });
-router.get('/', function(req, res, next) {
-    res.render('pizza', { title: "JO's Pizza", table: JSON.stringify(pizzasCreated) });
-});
+
 router.get('/addPizza', function(req, res, next){
     res.render('createPizza', { title: 'Crea tu propia pizza!'});
 });
-
-router.get('/thelist',function(req,err,res){
-    if(err) 
-        console.log("Error:  "+ err);
-    else {
-        console.log("Conexión a la BD realizada");
-        pizzap.find({}).toArray(function(err,result){
-            if(err){
-                res.send(err);
-            }
-            else if (result.length){
-                res.render('pizzalist',{pizzalist: result}); //un html que va a mostrar la lista de pizzas
-            }
-            else{
-                res.send("No documents found");
-            }
-        })
-        
-    }
-
-});
 router.post("/addPizza", function(req,res,next){
     let newPizza = req.body;
-    pizzap.insert(newPizza, function(err){
+    console.log("FORMATO PIZA: ", newPizza);
+    pizzap.insertMany(newPizza, function(err, result){
         if(err) {
             console.log("No fue posible conectarse",err);
             return;
         }else{
+            console.log("se insertó el elemento a la base",newPizza);
             res.status(200);
+            res.redirect('thelist');
+           
         }
     });    
-    //res.redirect("thelist");
+    
+});
+router.get('/thelist',function(req,res){
+    pizzap.find({}, function(err, pizzatolist){
+        if (err) throw err;
+        console.log("esto es lo que retorna: " ,pizzatolist);
+        actualPizzas = pizzatolist;
+        res.render("pizzalist", {title : "Tabla de pizzas ordenas", pizzalist : pizzatolist});
+    });
+   
+});
 
+router.delete('/:idPizza', function(req,res,next){
+    let currentPizza = actualPizzas[req.params.idPizza];
 });
 
 module.exports = router;
